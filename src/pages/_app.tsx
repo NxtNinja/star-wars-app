cd42f6d8544dcbb55a811d851cbbe1f805e42b7e
NxtNinja <136341993+NxtNinja@users.noreply.github.com>
2024-05-23 13:35:07 +0530

    tanstack query, nextui and font selection and setup done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Raleway } from "next/font/google";
+import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const raleway = Raleway({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <main className={raleway.className}>
+          <Component {...pageProps} />
+        </main>
+      </NextUIProvider>
+    </QueryClientProvider>
+  );
 }
